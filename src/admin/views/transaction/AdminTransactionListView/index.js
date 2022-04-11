a652a94fe35a7b1e470abe4b9a6ca82fef2056d1
orgtom78 <admin@originaate.capital>
2022-04-11 14:23:27 +0800

    v1.3

--- src/admin/views/transaction/AdminTransactionListView/index.js
+++ src/admin/views/transaction/AdminTransactionListView/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import clsx from "clsx";
 import {
@@ -9,15 +8,16 @@ import {
   Card,
   Chip,
   Container,
-  Checkbox,
+  Divider,
+  Grid,
   Table,
+  TableContainer,
   TableBody,
   TableCell,
-  TableContainer,
   TableHead,
   TablePagination,
   TableRow,
-  TableSortLabel,
+  TextField,
   Typography,
   ThemeProvider,
   StyledEngineProvider,
@@ -25,13 +25,21 @@ import {
 } from "@mui/material";
 import makeStyles from "@mui/styles/makeStyles";
 import PerfectScrollbar from "react-perfect-scrollbar";
-import Page from "src/components/Page";
 import * as queries from "src/graphql/queries.js";
-import { API, graphqlOperation } from "aws-amplify";
+import Page from "src/components/Page";
+import { Auth, API, graphqlOperation } from "aws-amplify";
 import moment from "moment";
 import getInitials from "src/utils/getInitials";
 import { green, orange, red } from "@mui/material/colors";
 import NumberFormat from "react-number-format";
+import LoaderButton from "src/components/LoaderButton.js";
+import { Search as SearchIcon } from "react-feather";
+import { onError } from "src/libs/errorLib.js";
+import { subDays } from "date-fns";
+
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import DesktopDatePicker from "@mui/lab/DesktopDatePicker";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -62,195 +70,103 @@ const AdminTransactionListView = () => {
   const [selectedCustomerIds, setSelectedCustomerIds] = useState([]);
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(0);
-
-  const [order, setOrder] = useState("asc");
-  const [orderBy, setOrderBy] = useState("status");
+  const [searchterm, setSearchterm] = useState("");
+  const [invoicestart, setInvoicestart] = useState(subDays(new Date(), 7));
+  const [invoiceend, setInvoiceend] = useState(new Date());
+  const [success, setSuccess] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    async function getRequests() {
-      const {
-        data: {
-          listRequests: { items: itemsPage1, nextToken },
-        },
-      } = await API.graphql(graphqlOperation(queries.listRequests));
-      const n = { data: { listRequests: { items: itemsPage1, nextToken } } };
-      const items = n.data.listRequests.items;
-      return items;
-    }
-
-    async function test() {
+    async function fetchData() {
       const c = await getRequests();
       const d = c.sort(function(a, b) {
         return new Date(b.createdAt) - new Date(a.createdAt);
       });
       setRequest(d);
     }
-    test();
+    fetchData();
   }, []);
 
-  function descendingComparator(a, b, orderBy) {
-    if (b[orderBy] < a[orderBy]) {
-      return -1;
-    }
-    if (b[orderBy] > a[orderBy]) {
-      return 1;
-    }
-    return 0;
-  }
-
-  function getComparator(order, orderBy) {
-    return order === "desc"
-      ? (a, b) => descendingComparator(a, b, orderBy)
-      : (a, b) => -descendingComparator(a, b, orderBy);
-  }
-
-  function stableSort(array, comparator) {
-    const stabilizedThis = array.map((el, index) => [el, index]);
-    stabilizedThis.sort((a, b) => {
-      const order = comparator(a[0], b[0]);
-      if (order !== 0) return order;
-      return a[1] - b[1];
-    });
-    return stabilizedThis.map((el) => el[0]);
-  }
-
-  const headCells = [
-    {
-      id: "buyers_name",
-      numeric: false,
-      disablePadding: false,
-      label: "Buyers  Name",
-    },
-    {
-      id: "suppliers_name",
-      numeric: false,
-      disablePadding: false,
-      label: "Suppliers Name",
-    },
-    { id: "amount", numeric: true, disablePadding: false, label: "Amount" },
-    { id: "status", numeric: false, disablePadding: false, label: "Status" },
-    {
-      id: "latest_update",
-      numeric: false,
-      disablePadding: false,
-      label: "Latest update",
-    },
-  ];
-
-  function EnhancedTableHead(props) {
+  async function getRequests() {
     const {
-      classes,
-      onSelectAllClick,
-      order,
-      orderBy,
-      numSelected,
-      rowCount,
-      onRequestSort,
-    } = props;
-    const createSortHandler = (property) => (event) => {
-      onRequestSort(event, property);
-    };
-
-    return (
-      <TableHead>
-        <TableRow>
-          <TableCell padding="checkbox">
-            <Checkbox
-              indeterminate={numSelected > 0 && numSelected < rowCount}
-              checked={rowCount > 0 && numSelected === rowCount}
-              onChange={onSelectAllClick}
-              inputProps={{ "aria-label": "select all desserts" }}
-            />
-          </TableCell>
-          {headCells.map((headCell) => (
-            <TableCell
-              key={headCell.id}
-              align={"left"}
-              padding={headCell.disablePadding ? "none" : "normal"}
-              sortDirection={orderBy === headCell.id ? order : false}
-            >
-              <TableSortLabel
-                active={orderBy === headCell.id}
-                direction={orderBy === headCell.id ? order : "asc"}
-                onClick={createSortHandler(headCell.id)}
-              >
-                {headCell.label}
-                {orderBy === headCell.id ? (
-                  <span className={classes.visuallyHidden}>
-                    {order === "desc"
-                      ? "sorted descending"
-                      : "sorted ascending"}
-                  </span>
-                ) : null}
-              </TableSortLabel>
-            </TableCell>
-          ))}
-        </TableRow>
-      </TableHead>
-    );
+      data: {
+        listRequests: { items: itemsPage1, nextToken },
+      },
+    } = await API.graphql(graphqlOperation(queries.listRequests));
+    const n = { data: { listRequests: { items: itemsPage1, nextToken } } };
+    const items = n.data.listRequests.items;
+    return items;
   }
 
-  EnhancedTableHead.propTypes = {
-    classes: PropTypes.object.isRequired,
-    numSelected: PropTypes.number.isRequired,
-    onRequestSort: PropTypes.func.isRequired,
-    onSelectAllClick: PropTypes.func.isRequired,
-    order: PropTypes.oneOf(["asc", "desc"]).isRequired,
-    orderBy: PropTypes.string.isRequired,
-    rowCount: PropTypes.number.isRequired,
-  };
+  async function handleSearch() {
+    setSuccess(false);
+    setLoading(true);
+    try {
+      const term = searchterm;
+      await searchRequest(term);
+    } catch (e) {
+      onError(e);
+    }
+    setSuccess(false);
+    setLoading(false);
+  }
 
-  const handleRequestSort = (event, property) => {
-    const isAsc = orderBy === property && order === "asc";
-    setOrder(isAsc ? "desc" : "asc");
-    setOrderBy(property);
-  };
-
-  const handleSelectAll = (event) => {
-    let newSelectedCustomerIds;
-
-    if (event.target.checked) {
-      newSelectedCustomerIds = request.map((request) => request.requestId);
+  async function searchRequest(input) {
+    if (input === "" || input === undefined) {
+      const c = await getRequests();
+      const d = c.sort(function(a, b) {
+        return new Date(b.createdAt) - new Date(a.createdAt);
+      });
+      setRequest(d);
     } else {
-      newSelectedCustomerIds = [];
+      const resterm = input.toLowerCase() + "*";
+      // search
+      const filter = {
+        invoice_number: {
+          wildcard: resterm,
+        },
+      };
+      const result = await API.graphql(
+        graphqlOperation(queries.searchRequests, { filter: filter })
+      );
+      if (result.data.searchRequests.items.length > 0) {
+        let array = await result.data.searchRequests.items;
+        const d = array.sort(function(a, b) {
+          return new Date(b.createdAt) - new Date(a.createdAt);
+        });
+        setRequest(d);
+      } else {
+        setRequest([]);
+      }
     }
+  }
 
-    setSelectedCustomerIds(newSelectedCustomerIds);
-  };
-
-  const handleSelectOne = (event, id) => {
-    const selectedIndex = selectedCustomerIds.indexOf(id);
-    let newSelectedCustomerIds = [];
-
-    if (selectedIndex === -1) {
-      newSelectedCustomerIds = newSelectedCustomerIds.concat(
-        selectedCustomerIds,
-        id
-      );
-    } else if (selectedIndex === 0) {
-      newSelectedCustomerIds = newSelectedCustomerIds.concat(
-        selectedCustomerIds.slice(1)
-      );
-    } else if (selectedIndex === selectedCustomerIds.length - 1) {
-      newSelectedCustomerIds = newSelectedCustomerIds.concat(
-        selectedCustomerIds.slice(0, -1)
-      );
-    } else if (selectedIndex > 0) {
-      newSelectedCustomerIds = newSelectedCustomerIds.concat(
-        selectedCustomerIds.slice(0, selectedIndex),
-        selectedCustomerIds.slice(selectedIndex + 1)
-      );
-    }
-
-    setSelectedCustomerIds(newSelectedCustomerIds);
-  };
+  async function filterRequests(input) {
+    const startDate = moment(input.invoicestart).format("YYYY-MM-DD");
+    const endDate = moment(input.invoiceend).format("YYYY-MM-DD");
+    let filter = {
+      invoice_date: { between: [startDate, endDate] },
+    };
+    const {
+      data: {
+        listRequests: { items: itemsPage1, nextToken },
+      },
+    } = await API.graphql(
+      graphqlOperation(queries.listRequests, { filter: filter })
+    );
+    const n = { data: { listRequests: { items: itemsPage1, nextToken } } };
+    const items = n.data.listRequests.items;
+    const d = items.sort(function(a, b) {
+      return new Date(b.createdAt) - new Date(a.createdAt);
+    });
+    setRequest(d);
+  }
 
   const handleLimitChange = (event) => {
-    setLimit(+event.target.value);
-    setPage(0);
+    setLimit(event.target.value);
   };
 
-  const handleChangePage = (event, newPage) => {
+  const handlePageChange = (event, newPage) => {
     setPage(newPage);
   };
 
@@ -270,16 +186,6 @@ const AdminTransactionListView = () => {
         <>
           <StyledEngineProvider injectFirst>
             <ThemeProvider theme={orangeTheme}>
-              <Chip label={request} color="secondary" />
-            </ThemeProvider>
-          </StyledEngineProvider>
-        </>
-      );
-    } else if (request === "Declined") {
-      return (
-        <>
-          <StyledEngineProvider injectFirst>
-            <ThemeProvider theme={redTheme}>
               <Chip label={request} color="secondary" />
             </ThemeProvider>
           </StyledEngineProvider>
@@ -306,15 +212,86 @@ const AdminTransactionListView = () => {
             <Card>
               <PerfectScrollbar>
                 <Box minWidth={1050}>
+                  <Divider />
+                  <br></br>
+                  <Grid
+                    container
+                    spacing={2}
+                    justifyContent="center"
+                    alignItems="center"
+                  >
+                    <Grid item xs={4}>
+                      <TextField
+                        fullWidth
+                        label="Search Invoice Number"
+                        name="Search"
+                        onChange={(e) => setSearchterm(e.target.value)}
+                        required
+                        value={searchterm || ""}
+                        variant="outlined"
+                        onKeyPress={(e) => {
+                          if (e.key === "Enter") {
+                            handleSearch(e.target.value);
+                          }
+                        }}
+                      />
+                    </Grid>
+                    <Grid item xs={2}>
+                      <LoaderButton
+                        startIcon={<SearchIcon />}
+                        disabled={loading}
+                        success={success}
+                        loading={loading}
+                        onClick={handleSearch}
+                      >
+                        Search
+                      </LoaderButton>
+                    </Grid>
+                    <Grid item xs={3}>
+                      <LocalizationProvider dateAdapter={AdapterDateFns}>
+                        <DesktopDatePicker
+                          value={invoicestart}
+                          label="Invoice Date From"
+                          onChange={(e) => setInvoicestart(e)}
+                          onAccept={(e) =>
+                            filterRequests({ invoicestart, invoiceend })
+                          }
+                          required
+                          renderInput={(params) => <TextField {...params} />}
+                        />
+                      </LocalizationProvider>
+                    </Grid>
+                    <Grid item xs={3}>
+                      <LocalizationProvider dateAdapter={AdapterDateFns}>
+                        <DesktopDatePicker
+                          value={invoiceend}
+                          label="Invoice Date To"
+                          onChange={(e) => setInvoiceend(e)}
+                          onAccept={(e) =>
+                            filterRequests({ invoicestart, invoiceend })
+                          }
+                          required
+                          renderInput={(params) => <TextField {...params} />}
+                        />
+                      </LocalizationProvider>
+                    </Grid>
+                  </Grid>
+                  <br></br>
+                  <Divider />
                   <TableContainer>
                     <Table>
-                      <EnhancedTableHead
-                        classes={classes}
-                        numSelected={selectedCustomerIds.length}
-                        onSelectAllClick={handleSelectAll}
-                        onRequestSort={handleRequestSort}
-                        rowCount={request.length}
-                      />
+                      <TableHead>
+                        <TableRow>
+                          <TableCell>Buyer's Name</TableCell>
+                          <TableCell>Suppliers' Name</TableCell>
+                          <TableCell>Invoice Number</TableCell>
+                          <TableCell>Amount</TableCell>
+                          <TableCell>Currency</TableCell>
+                          <TableCell>Status</TableCell>
+                          <TableCell>Invoice Date</TableCell>
+                          <TableCell>Invoice Due Date</TableCell>
+                        </TableRow>
+                      </TableHead>
                       <TableBody>
                         {request
                           .slice(page * limit, page * limit + limit)
@@ -330,19 +307,6 @@ const AdminTransactionListView = () => {
                                 }
                                 tabIndex={-1}
                               >
-                                <TableCell padding="checkbox">
-                                  <Checkbox
-                                    checked={
-                                      selectedCustomerIds.indexOf(
-                                        request.requestId
-                                      ) !== -1
-                                    }
-                                    onChange={(event) =>
-                                      handleSelectOne(event, request.requestId)
-                                    }
-                                    value="true"
-                                  />
-                                </TableCell>
                                 <TableCell>
                                   <Box alignItems="center" display="flex">
                                     <Link
@@ -364,6 +328,7 @@ const AdminTransactionListView = () => {
                                   </Box>
                                 </TableCell>
                                 <TableCell>{`${request.supplier_name}`}</TableCell>
+                                <TableCell>{`${request.invoice_number}`}</TableCell>
                                 <TableCell>
                                   <NumberFormat
                                     color="textPrimary"
@@ -374,11 +339,17 @@ const AdminTransactionListView = () => {
                                     prefix={"$"}
                                   />
                                 </TableCell>
+                                <TableCell>{`${request.invoice_currency}`}</TableCell>
                                 <TableCell>
                                   {checkstatus(request.request_status)}
                                 </TableCell>
                                 <TableCell>
-                                  {moment(request.createdAt).format(
+                                  {moment(request.invoice_date).format(
+                                    "DD/MM/YYYY"
+                                  )}
+                                </TableCell>
+                                <TableCell>
+                                  {moment(request.invoice_due_date).format(
                                     "DD/MM/YYYY"
                                   )}
                                 </TableCell>
@@ -393,7 +364,7 @@ const AdminTransactionListView = () => {
               <TablePagination
                 component="div"
                 count={request.length}
-                onPageChange={handleChangePage}
+                onPageChange={handlePageChange}
                 onRowsPerPageChange={handleLimitChange}
                 page={page}
                 rowsPerPage={limit}
